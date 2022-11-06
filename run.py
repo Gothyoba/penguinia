15c07ed449d7ce8bb4a4d04cd1ab53a2b9640937
gothyoba <leftraru.bg@proton.me>
2022-11-06 08:06:52 +0200

    Run.py comments

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from basic_input import ask_input
 
+#introduction
 def start():
 	print("You are a penguin.")
 	print("You live in the nation of Penguinia.")
+	#player input, see basic_input.py
 	ask_input()
 
+#runs the game
 if __name__ == '__main__':
     start()
