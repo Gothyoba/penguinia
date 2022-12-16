71e9fd85585917b23f988b1d6d752048c55db471
gothyoba <leftraru.bg@proton.me>
2022-12-16 16:45:46 +0200

    Removed how to play section from readme and added tutorial.

--- README.md
+++ README.md
@@ -4,30 +4,6 @@ Penguinia is a free and open source text-based exploration and combat game made 
 
 Penguinia is currently on version 0.0.4. In order to play penguinia, run the file run.py in a console capable of running python. There is currently no in-game tutorial.
 
-## How to play:
-
-Make sure to type everything in lowercase without any errors in order for the game to understand you. Always press enter after each command you give to the game. Here are all working commands:
-
-w - moves the player north
-
-a - moves the player west
-
-s - moves the player south
-
-d - moves the player east
-
-q - checks for quests
-
-e - ends the game
-
-y - starts a quest after the command q has been given
-
-n - opposite of yes
-
-o - attacks enemy while in combat
-
-p - sets your hitpoints to just under your maximum hitpoints in combat if you have a potion
-
 Each time you move the player the game will output the name of where you are, or a placeholder name. In the future this will be a more detailed description for each location.
 
 Penguinia currently limits the player to a 3 by 3 grid, however this grid will expand in the next few updates.
--- run.py
+++ run.py
@@ -1,12 +1,42 @@
 from basic_input import ask_input
+from error import *
 
 #introduction
 def start():
 	print("You are a penguin.")
 	print("You live in the nation of Penguinia.")
-	#tutorial to be added in 0.1
-	#player input, see basic_input.py
-	ask_input()
+	print("Your name is Morguin.")
+	print("You have inherited the position of underpriest from your mother and are relatavily wealthy.")
+	print("The seal kingdom to the north has been hostile to Penguinia for decades.")
+	print("In your time, it is unsure when war will arise.")
+	print("Do you want the tutorial? Type tut and press enter if you want the tutorial. If not, type no.")
+	key = input()
+	match key:
+		case "tut":
+			#tutorial
+			print("Make sure to type everything in lowercase without any errors. Always press enter after each command you give to the game. Here are all working commands:")
+			print("w - moves the player north")
+			print("a - moves the player west")
+			print("s - moves the player south")
+			print("d - moves the player east")
+			print("q - checks for quests")
+			print("e - ends the game")
+			print("y - starts a quest after the command q has been given")
+			print("n - opposite of yes")
+			print("o - attacks enemy while in combat")
+			print("p - sets your hitpoints to just under your maximum hitpoints in combat if you have a potion")
+			print("Each time you move around the world you will be given the name of a place and will have the opportunity to explore quests and combat.")
+			print("You always start the game with a sword and a potion.")
+			print("If you type something wrong, the game will simply reply with an error message and do nothing.")
+			print("Have fun playing penguinia!")
+		case "no":
+			print("Have fun playing penguinia!")
+			ask_input()
+		case "e":
+			break
+		#error
+		case _:
+			print(ERROR)
 
 #runs the game
 if __name__ == '__main__':
