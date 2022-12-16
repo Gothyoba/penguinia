e0dac5624f1c803b8465f6a1387947a7d4c712ee
Decio Battaglia <decio.battaglia@gmail.com>
2022-12-16 15:12:52 +0000

    restructured coordinates

--- basic_input.py
+++ basic_input.py
@@ -1,42 +1,24 @@
 import numpy as np
 from error import *
 #coordinates is currently unused
-from coordinates import NAMES, XMAX, YMAX, pos_to_index, QUESTS
+import coordinates as c
 from combat import combat1
 
-#directions and origin for grid
-ORIGIN = np.array([0,0])
-UP = np.array([0,1])
-RIGHT = np.array([1,0])
 
-
-#changes position and keeps player in grid
-def get_new_pos(pos, direction):
-	pos = pos + direction
-	if abs(pos[0]) > XMAX or abs(pos[1]) > YMAX:
-		print("Those lands are unknown. Who knows what dangers might lie in exploring them?")
-		return pos - direction
-	else:
-		return pos
-
-#gets names from names coordinates
-def get_names(pos):
-	index = pos_to_index(pos)
-	print(NAMES[index[0]][index[1]])
+def print_name(pos):
+    name = c.get_name(pos)
+    print("You are in " + name + ".")
 
 #gets quests from names coordinates
 def get_quests(pos):
-	index = pos_to_index(pos)
-	if QUESTS[index[0]][index[1]] == 0:
-		print("There are no quests here.")
-		return
-	print("There is the quest " + QUESTS[index[0]][index[1]].name + ". Do you want to play it?")
+	quest = c.get_quest(pos)
+	print("Here is the quest: " + quest.name + ". Do you want to play it?")
 	key = input()
 	match key:
 		#accepting is y for yes not accepting is n for no
 		case "y":
-			print(QUESTS[index[0]][index[1]].lore[0])
-			combat1(QUESTS[index[0]][index[1]].enemies[0], 5)
+			print(quest.lore)
+			combat1(quest.enemies[0], 5)
 		case "n":
 			ask_input()
 		#exiting
@@ -49,21 +31,20 @@ def get_quests(pos):
 
 #basic player input
 def ask_input():
-	pos = ORIGIN
+	pos = c.ORIGIN
 	while (True):
-		print(pos)
-		get_names(pos)
+		print_name(pos)
 		key = input()
 		match key:
 			#use of wasd for movement
 			case "w":
-				pos = get_new_pos(pos, UP)
+				pos = c.get_new_pos(pos, c.UP)
 			case "a":
-				pos = get_new_pos(pos, -RIGHT)
+				pos = c.get_new_pos(pos, c.LEFT)
 			case "s":
-				pos = get_new_pos(pos, -UP)
+				pos = c.get_new_pos(pos, c.DOWN)
 			case "d":
-				pos = get_new_pos(pos, RIGHT)
+				pos = c.get_new_pos(pos, c.RIGHT)
 			case "q":
 				get_quests(pos)
 			#ends the program
--- coordinates.py
+++ coordinates.py
@@ -2,27 +2,58 @@ import numpy as np
 from quests import Quest, LORE
 from enemy import ENEMIES
 
-XMAX = 1
-YMAX = 1
+forsteri = {
+        "name": "Forsteri",
+        "desc": "Forsteri is the capital of Penguinia.",
+        "quest": Quest(LORE[0], [ENEMIES[0]], "Free Penguinia Part 1")
+        }
 
-#coordinate matrix for place names
-NAMES = np.array(
-[["Placeholder", "Placeholder", "Placeholder"],
- ["Placeholder", "Forsteri is the capital of Penguinia.", "Placeholder"],
- ["Placeholder", "Placeholder", "Placeholder"]
+placeholder1 = {
+        "name": "Placeholder",
+        "desc": "Placeholder",
+        "quest": Quest(LORE[1], [ENEMIES[1]], "Free Penguinia Part 2")
+        }
+
+placeholder = {
+        "name": "Placeholder",
+        "desc": "Placeholder",
+        "quest": 0
+        }
+
+GRID = np.array(
+[[placeholder, placeholder1, placeholder],
+ [placeholder, forsteri, placeholder],
+ [placeholder, placeholder, placeholder]
 ]
 )
 
-#coordinate matrix for quests, some are placeholders, some don't work yet
-QUESTS = np.array(
-[[0, Quest(LORE[1], [ENEMIES[1]], "Free Penguinia Part 2"), 0],
- [0, Quest(LORE[0], [ENEMIES[0]], "Free Penguinia Part 1"), 0],
- [0, 0, 0]
-]
-)
+#directions and origin for grid
+XMAX = 2
+YMAX = 2
+ORIGIN = np.array([1,1])
+UP = np.array([0,1])
+RIGHT = np.array([1,0])
+DOWN = -UP
+LEFT = -RIGHT
 
-#changes from matrix indexing in numpy to a coordinate in the game
-def pos_to_index(pos):
-    row = pos[1] + YMAX
-    col = pos[0] + XMAX
-    return (row, col)
+def out_of_grid(pos):
+    print(pos)
+    return pos[0] > XMAX or pos[0] < 0 or pos[1] > YMAX or pos[1] < 0
+
+def get_name(pos):
+    return GRID[pos[1], pos[0]]["name"]
+
+def get_desc(pos):
+    return GRID[pos[1], pos[0]]["desc"]
+
+def get_quest(pos):
+    return GRID[pos[1], pos[0]]["quest"]
+
+#changes position and keeps player in grid
+def get_new_pos(pos, direction):
+    pos = pos + direction
+    if out_of_grid(pos):
+        print("Those lands are unknown. Who knows what dangers might lie in exploring them?")
+        return pos - direction
+    else:
+        return pos
