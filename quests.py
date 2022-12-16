fa68ac5b082b74eba57e9376043b4d3587ebf53b
gothyoba <leftraru.bg@proton.me>
2022-12-16 18:48:14 +0200

    Added quest description for second part of free penguinia

--- quests.py
+++ quests.py
@@ -6,4 +6,4 @@ class Quest:
 		self.name = name
 
 #lore texts shown before questing
-LORE = ["A priest has warned you of a violent seal intruder in the Grand Temple of Forsteri.", "Placeholder", "Placeholder"]
+LORE = ["A priest has warned you of a violent seal intruder in the Grand Temple of Forsteri.", "There appears to be a seal warrior claiming to be from the Seal Kingdom is ravaging the peaceful countryside of Penguinia.", "Placeholder"]
