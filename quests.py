d79d588db0a3ec5274cf9c82ded4768deca6d8bf
gothyoba <leftraru.bg@proton.me>
2023-01-07 14:47:40 +0200

    Significantly extended lore for quest Free Penguinia: Part 2

--- quests.py
+++ quests.py
@@ -1,6 +1,7 @@
 from reader import *
 
 part_01 = reader("./lore/free_penguinia_01")
+part_02 = reader("./lore/free_penguinia_02")
 
 #used to create different quests
 class Quest:
@@ -11,7 +12,7 @@ class Quest:
 
 #lore texts shown before questing
 LORE = [part_01, 
-	"There appears to be a seal warrior claiming to be from the Seal Kingdom is ravaging the peaceful countryside of Penguinia.", 
+	part_02, 
 	"An army of seals from the Seal Kingdom is attacking the port. It would appear as though the invasion has begun. You must try to escape alive.",
 	"You randomly found yourself in the city of Random after escaping in a small raft. President Froggy wants you to randomly kill some invading seals before going to Anura.",
 	"You now find yourself in Anura. You decide that you will destroy King Krow. Perhaps you should go to Monachinae to do that? A Seal Warrior starts quickly attacking you.",
