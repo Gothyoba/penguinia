f8ce215b1d85193106a319ef07b6a9cda2c08ed9
Gothyoba <leftraru.bg@proton.me>
2023-05-07 14:25:41 +0100

    added dialogue to quest free penguinia part 6

--- quests.py
+++ quests.py
@@ -5,6 +5,7 @@ part_02 = reader("./lore/free_penguinia/free_penguinia_02")
 part_03 = reader("./lore/free_penguinia/free_penguinia_03")
 part_04 = reader("./lore/free_penguinia/free_penguinia_04")
 part_05 = reader("./lore/free_penguinia/free_penguinia_05")
+part_06 = reader("./lore/free_penguinia/free_penguinia_06")
 
 #used to create different quests
 class Quest:
@@ -19,6 +20,6 @@ LORE = [part_01,
 	part_03,
 	part_04,
 	part_05,
-	"You must now flee the asassins 🗡️ in the port as you try to reach Krow's Castle in Pinniped. Perhaps you should go to Odobidae first? You should be able to grab a snack.",
+	part_06,
 	"Oh no! A seal is attacking you at the market. You must defend yourself.",
 	"You have reached Pinniped. You must now face the King. 👑"]
