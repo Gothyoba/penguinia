3a07e74e5f99cec81051f54cfc57853520333132
Gothyoba <leftraru.bg@proton.me>
2024-06-27 11:08:49 +0100

    both reader functions use utf-8 encoding

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 #used to parse and show text files in-game
 def reader(file):
-	contents = open(file, "r").read()
+	contents = open(file, "r", encoding="utf-8").read()
 	return contents
 #used for indivdual lines
 def linereader(file,line):
